ecd5324fed37dc18f4614904cc582df392ace0fb
yt8492 <tmymyut2000@gmail.com>
2020-02-21 21:43:33 +0900

    implement empty check

--- backend/ReiwaFakeNews/modules/usecase/src/main/kotlin/com/zli/usecase/savenews/SaveNewsResult.kt
+++ backend/ReiwaFakeNews/modules/usecase/src/main/kotlin/com/zli/usecase/savenews/SaveNewsResult.kt
@@ -2,6 +2,7 @@ package com.zli.usecase.savenews
 
 import com.zli.domain.model.News
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 sealed class SaveNewsResult {
 
@@ -9,5 +10,15 @@ sealed class SaveNewsResult {
     data class Success(val news: News) : SaveNewsResult()
 
     @Serializable
-    data class Failure(val message: String) : SaveNewsResult()
+    sealed class Failure(val message: String) : SaveNewsResult() {
+
+        @Serializable
+        class EmptyForm : Failure("フォームが空です")
+
+        @Serializable
+        class Unknown(
+            @Transient
+            private val exception: Exception? = null
+        ) : Failure(exception?.message ?: "不明なエラーです")
+    }
 }
